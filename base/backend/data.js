0abd8591e70aae744c3f8f98dd47864df863201b
John Lovato <53193607+JTLovato@users.noreply.github.com>
2023-11-12 22:28:54 -0500

    Working through 23, uploading users

--- base/backend/data.js
+++ base/backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "jtlovato",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Miguel",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: "1",
